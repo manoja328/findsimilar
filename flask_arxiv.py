b98209698650f228e3ddbd9eb2b701c424647afa
manoja328 <manoja328@gmail.com>
2023-01-31 13:25:58 -0500

    add TSNE plot

--- flask_arxiv.py
+++ flask_arxiv.py
@@ -12,15 +12,29 @@ from jinja2 import Template
 import pickle
 import numpy as np
 import pandas as pd
+import plotly.express as px
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.manifold import TSNE
+
+app = Flask(__name__)
 
 ## calculate the cosine similarity between a vector and a numpy array of vectors
 cosine_similarity = lambda a, b: np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 # get top-k sorted indices in descending order
 top_k = lambda a, k: np.argsort(-a)[:k]
 
-app = Flask(__name__)
+def get_tsne(embeddings, n_components=3, perplexity=30, n_iter=250):
+    """
+    This function takes in the embeddings as input, and returns the t-SNE transformed output.
+    The n_components parameter specifies the number of dimensions in the t-SNE output.
+    The perplexity parameter controls the balance between local and global structure in the t-SNE output.
+    The n_iter parameter specifies the number of iterations for t-SNE optimization.
+
+    """
+    tsne = TSNE(n_components=n_components, perplexity=perplexity, n_iter=n_iter)
+    return tsne.fit_transform(embeddings)
+
 
 with open("vector_np.data","rb") as f:
     vector_np = pickle.load(f)
@@ -70,8 +84,7 @@ def search():
         if weight > 0.001:
             tokens.append( {"name":feature_names[index],"weight": weight} )
         
-    print("top tokens: ",tokens)
-    
+    #print("top tokens: ",tokens)
     similarities = find_similar(query_vector)
     if not len(similarities):
         matched_ids = range(K)
@@ -94,7 +107,63 @@ def search():
 @app.route('/', methods=['GET'])
 def index():
     return render_template('main.html')
-  
+
+
+def plot_tsne3d():
+    # Load your embeddings
+    N = 100
+    
+    # if the plot has already been generated, just load it    
+    embeddings = vector_np[:N]
+    
+    # Perform t-SNE on the embeddings
+    tsne_data = get_tsne(embeddings,n_components=3)
+
+    tsne_df = pd.DataFrame(tsne_data, columns=['x', 'y','z'])
+
+    # Combine the t-SNE result and metadata into a pandas dataframe
+    df = pd.concat((tsne_df, docs_df[:N]), axis=1)
+    
+    # Create a 3D plot using Plotly
+    fig = px.scatter_3d(df, x='x', y='y', z='z', hover_data=['id','title'])
+
+    # Render the plot in an HTML template
+    #return render_template("plot.html", plot=fig.to_html(full_html=False))
+    fig.write_html("templates/plot.html", full_html=True)
+    return render_template("plot.html")
+
+@app.route("/plot")
+def plot_tsne2d():
+    # Load your embeddings
+    N = 5000
+    
+    # if the plot has already been generated, just load it
+    try:
+        return render_template("plot.html")
+    except:
+        print("rendering a new TSNE plot")
+    
+    # if the plot has already been generated, just load it    
+    embeddings = vector_np[:N]
+    
+    # Perform t-SNE on the embeddings
+    tsne_data = get_tsne(embeddings,n_components=2)
+
+    tsne_df = pd.DataFrame(tsne_data, columns=['x', 'y'])
+
+    # Combine the t-SNE result and metadata into a pandas dataframe
+    df = pd.concat((tsne_df, docs_df[:N]), axis=1)
+    
+    # Create a 3D plot using Plotly
+    fig = px.scatter(df, x='x', y='y', hover_data=['id','title'])
+
+    # Render the plot in an HTML template
+    #return render_template("plot.html", plot=fig.to_html(full_html=False))
+    fig.write_html("templates/plot.html", full_html=True)
+    return render_template("plot.html")
+
+
+
 def main():
     app.run(host='0.0.0.0', port=5000,debug=True)
 
